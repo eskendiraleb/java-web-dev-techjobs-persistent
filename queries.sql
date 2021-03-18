294e81f19bdd4c2c6ef75f5b28bbf279a2f7bc43
Eskindir <eskindiraleb@yahoo.com>
2021-03-18 06:28:31 -0500

    assignment 4 close to final

--- queries.sql
+++ queries.sql
@@ -1,7 +1,13 @@
 ## Part 1: Test it with SQL
-
+create table job (
+    id int AUTO_INCREMENT primary key,
+    name varchar(100),
+    skill varchar(255),
+    employer_id int,
+    jobs_id int
+      );
 ## Part 2: Test it with SQL
-
+SELECT * FROM employer WHERE (location = "St Louis City");
 ## Part 3: Test it with SQL
-
+drop table job
 ## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/EmployerController.java
@@ -1,6 +1,8 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
 import org.launchcode.javawebdevtechjobspersistent.models.Employer;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -13,11 +15,20 @@ import java.util.Optional;
 @RequestMapping("employers")
 public class EmployerController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @GetMapping
+    public String displayAllEmployers(Model model) {
+        model.addAttribute("title","AllEmployers");
+        model.addAttribute("employers",employerRepository.findAll());
+        return "employers/index";
+    }
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
         model.addAttribute(new Employer());
-        return "employers/add";
+            return "employers/add";
     }
 
     @PostMapping("add")
@@ -25,18 +36,20 @@ public class EmployerController {
                                     Errors errors, Model model) {
 
         if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Employer");
+            model.addAttribute("employers", employerRepository.findAll());
             return "employers/add";
         }
-
+        employerRepository.save(newEmployer);
         return "redirect:";
     }
 
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional<Employer> optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
-            Employer employer = (Employer) optEmployer.get();
+            Employer employer = optEmployer.get();
             model.addAttribute("employer", employer);
             return "employers/view";
         } else {
